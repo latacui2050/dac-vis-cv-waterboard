6ff2be805d5ca468d08af1c7c577d5369bceb9cd
Wenmo Cui <wenmo_cui@berkeley.edu>
2024-04-22 08:34:45 -0700

    change the settings for vite/typescript

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   server: {
     hmr:
       process.env.CODESANDBOX_SSE || process.env.GITPOD_WORKSPACE_ID
         ? 443
         : undefined,
   },
+  base: "/dac-vis-cv-waterboard/"
 });
